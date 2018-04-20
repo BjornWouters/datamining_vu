eff89936f0ba345c3cb0ca70cc005479f41ce941
amberhendriks <ahproefdedag@gmail.com>
2018-04-20 18:03:05 +0200

    Update model.py

--- scripts/model.py
+++ scripts/model.py
@@ -5,7 +5,8 @@ import os
 import glob
 import pandas as pd
 from sklearn.model_selection import TimeSeriesSplit
-from sklearn.linear_model import ElasticNet
+from sklearn.linear_model import ElasticNetCV
+from sklearn.metrics import r2_score
 
 def import_data(ID):
     df = pd.read_csv(ID, index_col=0)
@@ -18,22 +19,22 @@ def split_test_set(sample_data):
     
     return test_set, training_set
     
-def split_data(sample_data):
+def split_data(sample_data, test_set):
    #split in test and trainingset and timeseries cross validation
    X = sample_data.loc[:,"activity"::]
    y = sample_data.mood
-
+   X_test = test_set.loc[:,"activity"::]
+   y_test = test_set.mood
    tscv = TimeSeriesSplit(max_train_size=None, n_splits=11)
-   
    #NOG BEKIJKEN, raise KeyError('%s not in index' % objarr[mask]) KeyError: '[ 0  1  2  3  4  5  6  7  8  9 10 11 12 13] not in index'
-   """for train_index, test_index in tscv.split(X):
-       print("TRAIN:", train_index, "TEST:", test_index)
-       X_train, X_test = X[train_index], X[test_index]
-       y_train, y_test = y[train_index], y[test_index]"""
+   #for train_index, test_index in tscv.split(X):
+   #    print("TRAIN:", train_index, "TEST:", test_index)
+   #    X_train, X_test = X[train_index], X[test_index]
+   #    y_train, y_test = y[train_index], y[test_index]
        
    #Nog bekijken --> NaN pikt 'ie niet, alle NaN veranderen naar 0 kan lijkt mij niet overal?
-   elastic_model = elasticnet(X,y)
-   recurrent_model = recurrent_neural_network(X,y)
+   prediction_elasticnet = elasticnet(X,y,X_test, y_test)
+   #recurrent_model = recurrent_neural_network(X,y)
 
 def benchmark(test_set, training_set, y_prediction, y_true):
     y_true.append(test_set.mood[-1])
@@ -41,13 +42,11 @@ def benchmark(test_set, training_set, y_prediction, y_true):
     return y_prediction, y_true
 
 
-def elasticnet(X,y):
-    regr = ElasticNet(random_state=0)   
-    regr.fit(X, y)
-    ElasticNet(alpha=1.0, copy_X=True, fit_intercept=True, l1_ratio=0.5, max_iter=1000, normalize=False, positive=False, precompute=False,random_state=0, selection='cyclic', tol=0.0001, warm_start=False)
-    print(regr.coef_, regr.intercept_)
-    
-    return #model?
+def elasticnet(X,y,X_test,y_test): #heb nu even cv gedaan in de elasticnet, kunnen we veradenren als timeseriesspplit werkt (=3x CV verwijderen)
+    regr = ElasticNetCV(cv=5, random_state=0)   
+    y_prediction_elasticnet = regr.fit(X, y).predict(X_test)
+
+    return y_prediction_elasticnet
 
 def recurrent_neural_network(X,y):
     #
@@ -56,7 +55,7 @@ def recurrent_neural_network(X,y):
     return #model?
 
 #main program 
-os.chdir( '/Users/amber/Desktop/data_mining' )
+os.chdir( '/Users/amber/Desktop/data_mining/results' )
 filenames = glob.glob( '*/**.csv' )
 benchmark_y_prediction = []
 benchmark_y_true = []
@@ -65,7 +64,7 @@ for sample_id in filenames:
     sample_data = import_data(sample_id)
     test_set, training_set = split_test_set(sample_data)
     benchmark_y_prediction, benchmark_y_true = benchmark(test_set, training_set, benchmark_y_prediction, benchmark_y_true)
-    #split_data(training_set) #voor de timeseriessplit, zodat daarna de modellen gemaakt kunnen worden.
+    split_data(training_set, test_set) #voor de timeseriessplit, zodat daarna de modellen gemaakt kunnen worden.
 #
 #modellen testen en AUC uitrekenen, voor alle modellen (voor benchmark dus y_prediction met y_test vergelijken)
 #
