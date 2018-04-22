ed268a7b7beb6300ab19f16024dab2dacea6b03d
amberhendriks <ahproefdedag@gmail.com>
2018-04-22 08:38:47 +0200

    Update model.py

--- scripts/model.py
+++ scripts/model.py
@@ -35,21 +35,21 @@ def split_data(sample_data, test_set):
    #    y_train, y_test = y[train_index], y[test_index]
        
    #Nog bekijken --> NaN pikt 'ie niet, alle NaN veranderen naar 0 kan lijkt mij niet overal?
-   prediction_elasticnet = elasticnet(X,y,X_test, y_test)
-   prediction_recurrent_nn = recurrent_neural_network(X,y, X_test, y_test)
+   
+   return X, y, X_test, y_test
 
 def benchmark(test_set, training_set, y_prediction, y_true):
     y_true.append(test_set.mood[-1])
     y_prediction.append(training_set[-1:].mood[-1])
     return y_prediction, y_true
 
-def elasticnet(X,y,X_test,y_test): #heb nu even cv gedaan in de elasticnet, kunnen we veradenren als timeseriesspplit werkt (=3x CV verwijderen)
+def elasticnet(X,y,X_test): #heb nu even cv gedaan in de elasticnet, kunnen we veradenren als timeseriesspplit werkt (=3x CV verwijderen)
     regr = ElasticNetCV(cv=5, random_state=0)   
     y_prediction_elasticnet = regr.fit(X, y).predict(X_test)
     #fpr, tpr = roc_curve(y_test, y_prediction_elasticnet, sample_weight=None, drop_intermediate=True)
     return y_prediction_elasticnet
 
-def recurrent_neural_network(X,y, X_test, y_test):
+def recurrent_neural_network(X,y, X_test):
     regr = MLPRegressor(hidden_layer_sizes=(100,))
     regr.fit(X,y)
     y_prediction_nn = regr.predict(X_test)
@@ -61,12 +61,16 @@ os.chdir( '/Users/amber/Desktop/data_mining/results' )
 filenames = glob.glob( '*/**.csv' )
 benchmark_y_prediction = []
 benchmark_y_true = []
+elasticnet_y_prediction = []
+recurrentnn_y_prediction = []
 
 for sample_id in filenames:
     sample_data = import_data(sample_id)
     test_set, training_set = split_test_set(sample_data)
     benchmark_y_prediction, benchmark_y_true = benchmark(test_set, training_set, benchmark_y_prediction, benchmark_y_true)
-    split_data(training_set, test_set) #voor de timeseriessplit, zodat daarna de modellen gemaakt kunnen worden.
-#
-#modellen testen en AUC uitrekenen, voor alle modellen (voor benchmark dus y_prediction met y_test vergelijken)
-#
+    X, y, X_test, y_test = split_data(training_set, test_set) 
+    elasticnet_y_prediction.append(elasticnet(X,y,X_test))
+    recurrentnn_y_prediction.append(recurrent_neural_network(X,y, X_test))
+
+
+print(benchmark_y_prediction, elasticnet_y_prediction, recurrentnn_y_prediction)
