3fc900dce2dffb7daa6484c78050281600742cfe
bjornwouters <bjorn-wouters@hotmail.com>
2018-05-24 18:08:07 +0200

    Final feature selection

--- assignment_2/scripts/models.py
+++ assignment_2/scripts/models.py
@@ -12,19 +12,24 @@ from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 
+
+
 def import_data():
-    training_set = pd.read_csv("../results/prepared_train.csv", sep=',')
-    original_dataset = pd.read_csv("../results/predict_dataset.csv", sep=',')
+    training_set = pd.read_csv("../results/kaggle_train.csv", sep=',')
+    original_dataset = pd.read_csv("../results/kaggle_dataset.csv", sep=',')
     dropped_features = ['prop_starrating', 'position', 'click_bool', 'booking_bool']
-    X = training_set.drop(dropped_features, axis=1)
-    y = training_set.booking_bool
+    X = training_set.drop(dropped_features, axis=1).fillna(0)
+    y = training_set.click_bool
     X_train, X_validation, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    test_set = pd.read_csv("../results/test_set.csv", sep=',', nrows=100)
+    test_set = pd.read_csv("../results/kaggle_test.csv", sep=',').fillna(0)
+    test_set = test_set.drop(['prop_starrating'], axis=1)
     return X_train, y_train, X_validation, original_dataset, test_set
 
 
 def GBM(X, y):
-    clf = GradientBoostingRegressor(random_state=None, n_estimators=1500, max_depth=2, min_samples_split=2, learning_rate=0.01, loss='quantile', alpha=0.7)
+    clf = GradientBoostingRegressor(random_state=None, n_estimators=1500, max_depth=2,
+                                    min_samples_split=2, learning_rate=0.01,
+                                    loss='quantile', alpha=0.7)
     clf.fit(X, y)
     print('Feature importances GBM: {}'.format(clf.feature_importances_))
     
@@ -32,7 +37,8 @@ def GBM(X, y):
 
 
 def RF(X, y):
-    clf = RandomForestRegressor(n_estimators=10, max_depth=None, min_samples_split=2, random_state=4, max_features='sqrt', n_jobs=1)
+    clf = RandomForestRegressor(n_estimators=10, max_depth=None, min_samples_split=2,
+                                random_state=4, max_features='sqrt', n_jobs=1)
     clf.fit(X, y)
     print('Feature importances RF: {}'.format(clf.feature_importances_))
     
@@ -44,6 +50,7 @@ def predictions(original_dataset, test_set, clfrf, clfgbm):
     predictionsrf = list()
     predictionsgbm = list()
     used_properties = list()
+
     for i, id in enumerate(srch_ids):
         listings = original_dataset[original_dataset.srch_id == id]
         test_row = test_set[test_set.prop_id.isin(list(listings.prop_id))]
@@ -56,10 +63,11 @@ def predictions(original_dataset, test_set, clfrf, clfgbm):
         outputgbm = clfgbm.predict(test_row)
         predictionsrf.append(outputrf)
         predictionsgbm.append(outputgbm)
-        if i == 2000:
+        if i == 5000:
             break
-    rf='RF'
-    gbm='GBM'
+
+    rf = 'RF'
+    gbm = 'GBM'
     nDCG_RF = nDCG(predictionsrf, used_properties, original_dataset, rf)
     nDCG_GBM = nDCG(predictionsgbm, used_properties, original_dataset, gbm)
 
@@ -70,33 +78,33 @@ def nDCG(predictions, used_properties, original_dataset, method):
     random_score_list = list()
     final_score_list = list()
     srch_ids = original_dataset.srch_id.unique()
-    for i, id in enumerate(srch_ids):
-        listings = original_dataset[original_dataset.srch_id == id]
-        listings = listings[listings.prop_id.isin(list(used_properties[i]))]
-        predicted_index = np.argsort(predictions[i])
-        if len(listings) != len(predicted_index):
-            break
-        listings['predicted_index'] = predicted_index
-        score = calculate_score(listings.sort_values('predicted_index'))
-        if score == 0:
-            final_score = 0
-        else:
-            max_score = calculate_score(listings)
-            final_score = score / max_score
-        final_score_list.append(final_score)
+    with open('../results/test_output.txt', 'w') as test_output:
+        for i, id in enumerate(srch_ids):
+            listings = original_dataset[original_dataset.srch_id == id]
+            listings = listings[listings.prop_id.isin(list(used_properties[i]))]
+            predicted_index = np.argsort(predictions[i])
+            if len(listings) != len(predicted_index):
+                break
+            listings['predicted_index'] = predicted_index
+            score = calculate_score(listings.sort_values('predicted_index'), id, test_output)
+            if score == 0:
+                final_score = 0
+            else:
+                max_score = calculate_score(listings, None, None)
+                final_score = score / max_score
+            final_score_list.append(final_score)
+            # Calculate random shuffle scores
+            listings = listings.sample(frac=1)
+            score = calculate_score(listings, None, None)
+            if score == 0:
+                final_score = 0
+            else:
+                max_score = calculate_score(listings, None, None)
+                final_score = score / max_score
+            random_score_list.append(final_score)
 
-        # Calculate random shuffle scores
-        listings = listings.sample(frac=1)
-        score = calculate_score(listings)
-        if score == 0:
-            final_score = 0
-        else:
-            max_score = calculate_score(listings)
-            final_score = score / max_score
-        random_score_list.append(final_score)
-
-        if i == 2000:
-            break
+            if i == 5000:
+                break
 
     mean_final_scores = np.mean(final_score_list)
     print('Final score {}: {}'.format(method, mean_final_scores))
@@ -105,10 +113,12 @@ def nDCG(predictions, used_properties, original_dataset, method):
     return mean_final_scores
 
 
-def calculate_score(listings):
+def calculate_score(listings, id, test_output):
     sum_score = 0
     for i, row in enumerate(listings.iterrows(), start=1):
         index, listing = row
+        if test_output:
+            test_output.write(str(int(listing.srch_id)) + ', ' + str(int(listing.prop_id)) + '\n')
         score = 0
         if listing.booking_bool == 1:
             score = 5
@@ -132,7 +142,7 @@ def max_scores(df):
     for i, id in enumerate(srch_ids):
         listings = df[df.srch_id == id]
         sorted_listings = listings.sort_values(by=['booking_bool', 'click_bool'], ascending=False)
-        score = calculate_score(sorted_listings)
+        score = calculate_score(sorted_listings, None, None)
 
         id_list.append(id)
         score_list.append(score)
@@ -145,53 +155,37 @@ def max_scores(df):
 
 def predict_test_set(clf, test_set):
     srch_ids = test_set.srch_id.unique()
-    predictions_test_set= list()
-    used_properties = list()
-    dropped_features = ['prop_id', 'srch_id']
-    for i, id in enumerate(srch_ids):
-        listings = test_set[test_set.srch_id == id]
-        test_row = test_set[test_set.prop_id.isin(list(listings.prop_id))]
-        used_properties.append(test_row.prop_id)
-        test_row = test_row.drop(dropped_features, axis=1)
-        if len(test_row) == 0:
-            continue
+    dropped_features = ['srch_id', 'prop_id']
+    with open('../results/submission.txt', 'w') as submission:
+        submission.write('SearchId,PropertyId' + '\n')
+        for i, id in enumerate(srch_ids):
+            listings = test_set[test_set.srch_id == id]
+            output_test_set = clf.predict(listings.drop(dropped_features, axis=1))
+            predicted_index = np.argsort(output_test_set)
+            listings['predicted_index'] = predicted_index
+            listings = listings.sort_values('predicted_index')
+            for i, row in enumerate(listings.iterrows(), start=1):
+                index, listing = row
+                submission.write(
+                    str(int(listing.srch_id)) + ',' + str(int(listing.prop_id)) + '\n')
 
-        output_test_set = clf.predict(test_row)
-        predictions_test_set.append(output_test_set)
-        if i == 100:
-            break
-    submission_file(predictions_test_set, used_properties, test_set)
-    
-    return
 
-def submission_file(predictions, used_properties, test_set):
-    srch_ids = test_set.srch_id.unique()
-    file = pd.DataFrame()
-    #df2 = pd.DataFrame(columns=df1['Column header'])
-    for i, id in enumerate(srch_ids):
-        listings = test_set[test_set.srch_id == id]
-        listings = listings[listings.prop_id.isin(list(used_properties[i]))]
-        predicted_index = np.argsort(predictions[i])
-        if len(listings) != len(predicted_index):
-            break
-        listings['predicted_index'] = predicted_index
-        file = file.append(listings.sort_values(by=['predicted_index']))
-        if i == 100:
-            break
-    print(file)
-    print(listings.columns)
-    file.columns = listings.columns
-    header = ['srch_id', 'prop_id']
-    file.to_csv('../results/predictions_group_26.csv', sep=',', index=False, columns=header)
+def main():
+    # Set useless warning to None
+    pd.options.mode.chained_assignment = None
 
-# main program
-X_train, y_train, X_validation, original_dataset, test_set = import_data()
-clfgbm = GBM(X_train.drop('prop_id', axis=1), y_train)
-clfrf = RF(X_train.drop('prop_id', axis=1), y_train)
-nDCG_RF, nDCG_GBM = predictions(original_dataset, X_validation, clfrf, clfgbm)
+    # main program
+    X_train, y_train, X_validation, original_dataset, test_set = import_data()
+    # clfgbm = GBM(X_train.drop('prop_id', axis=1), y_train)
+    clfrf = RF(X_train.drop('prop_id', axis=1), y_train)
+    # nDCG_RF, nDCG_GBM = predictions(original_dataset, X_validation, clfrf, clfgbm)
 
-if nDCG_RF > nDCG_GBM:
+    # if nDCG_RF > nDCG_GBM:
     predict_test_set(clfrf, test_set)
-else:
-    predict_test_set(clfgbm, test_set)
-#predict test set with the best model
+    # else:
+    #     predict_test_set(clfgbm, test_set)
+    # predict test set with the best model
+
+
+if __name__ == '__main__':
+    main()
--- assignment_2/scripts/preparation.py
+++ assignment_2/scripts/preparation.py
@@ -1,19 +1,25 @@
 import pandas as pd
 
 
-def normalize_values(df):
-    averaged_df = df.groupby(['prop_id']).mean().fillna(0)
+def normalize_values(df, test=False):
+    if not test:
+        averaged_df = df.groupby(['prop_id']).mean().fillna(0)
+    else:
+        averaged_df = df.fillna(0)
+
     for name, series in averaged_df.iteritems():
-        if name in ['click_bool', 'booking_bool']:
+        if name in ['click_bool', 'booking_bool', 'promotion_flag',
+                    'prop_brand_bool', 'srch_id', 'prop_id']:
             continue
         averaged_df[name] = ((averaged_df[name] -
                               averaged_df[name].mean()) /
                              averaged_df[name].std(ddof=0))
 
     # Don't use srch_ids where there is no booking realized
-    averaged_df = averaged_df[averaged_df.booking_bool != 0]
+    if not test:
+        averaged_df = averaged_df[averaged_df.booking_bool != 0]
 
-    return averaged_df
+    return averaged_df.fillna(0)
 
 
 def import_data(filename):
@@ -21,25 +27,55 @@ def import_data(filename):
         'prop_id', 'prop_starrating', 'prop_review_score',
         'prop_brand_bool', 'prop_location_score1', 'prop_location_score2',
         'prop_log_historical_price', 'position', 'price_usd', 'promotion_flag',
-        'click_bool', 'booking_bool'
+        'srch_room_count', 'srch_children_count', 'srch_adults_count',
+        'visitor_hist_adr_usd', 'visitor_hist_starrating', 'click_bool',
+        'booking_bool'
     ]
     df = pd.read_csv(filename, usecols=fields)
-    fields1 = ['srch_id', 'prop_id', 'click_bool', 'booking_bool']
+    fields1 = ['srch_id', 'prop_id', 'click_bool', 'booking_bool',
+               'visitor_hist_starrating', 'visitor_hist_adr_usd']
     df1 = pd.read_csv(filename, usecols=fields1).fillna(0)
     return df, df1
 
+
 def import_test_data(filename):
-    fields = ['srch_id','prop_id', 'prop_review_score','prop_brand_bool', 'prop_location_score1', 'prop_location_score2','prop_log_historical_price', 'price_usd', 'promotion_flag']
-    df = pd.read_csv(filename, usecols=fields).fillna(0)
+    fields = [
+        'srch_id', 'prop_id', 'prop_starrating', 'prop_review_score',
+        'prop_brand_bool', 'prop_location_score1', 'prop_location_score2',
+        'prop_log_historical_price', 'price_usd', 'promotion_flag',
+        'srch_room_count', 'srch_children_count', 'srch_adults_count',
+        'visitor_hist_adr_usd', 'visitor_hist_starrating'
+    ]
+    df = pd.read_csv(filename, usecols=fields)
     return df
 
+
+def feature_selection(df):
+    # Relative price in comparison with the star rating
+    df['rating_price'] = df['prop_starrating']/df['price_usd']
+
+    # Price differencevisitor_hist_starrating
+    df['price_difference'] = df['price_usd']-df['visitor_hist_adr_usd']
+
+    # Rating differences
+    df['rate_differences'] = df['prop_review_score']-df['visitor_hist_starrating']
+
+    return df
+
+
 def main():
-    df, df1 = import_data('../data/training_set_VU_DM_2014.csv')
-    test_set = import_test_data('../data/test_set_VU_DM_2014.csv')
-    df1.to_csv('../results/predict_dataset.csv', index=False)
-    test_set.to_csv('../results/test_set.csv', index=False)
-    df = normalize_values(df)
-    df.to_csv('../results/prepared_train.csv')
+    # df, df1 = import_data('../data/kaggle_train.csv')
+    test_set = import_test_data('../data/kaggle_test.csv')
+
+    # df = feature_selection(df)
+    # df = normalize_values(df)
+
+    test_set = feature_selection(test_set)
+    test_set = normalize_values(test_set, test=True)
+
+    test_set.to_csv('../results/kaggle_test.csv', index=False)
+    # df.to_csv('../results/kaggle_train.csv')
+    # df1.to_csv('../results/kaggle_dataset.csv', index=False)
 
 
 if __name__ == '__main__':
