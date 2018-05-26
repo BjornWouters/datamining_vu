9e206adc2b62fdf59e7578b51776d0fb8bd1a6f1
amberhendriks <ahproefdedag@gmail.com>
2018-05-26 22:20:57 +0200

    final version

--- assignment_2/scripts/models.py
+++ assignment_2/scripts/models.py
@@ -1,47 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import glob
+
+
 import numpy as np
-import math
-import matplotlib.pyplot as plt
 import pandas as pd
-from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
-from sklearn.metrics import accuracy_score
+import matplotlib.pyplot as plt
+from sklearn.externals import joblib
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 
 
 
 def import_data():
-    training_set = pd.read_csv("../results/kaggle_train.csv", sep=',')
-    original_dataset = pd.read_csv("../results/kaggle_dataset.csv", sep=',')
+    training_set = pd.read_csv("../results/prepared_train.csv", sep=',')
+    original_dataset = pd.read_csv("../results/predict_dataset.csv", sep=',')
     dropped_features = ['prop_starrating', 'position', 'click_bool', 'booking_bool']
     X = training_set.drop(dropped_features, axis=1).fillna(0)
     y = training_set.click_bool
     X_train, X_validation, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=42)
-    test_set = pd.read_csv("../results/kaggle_test.csv", sep=',').fillna(0)
+    test_set = pd.read_csv("../results/test_set.csv", sep=',').fillna(0)
     test_set = test_set.drop(['prop_starrating'], axis=1)
     return X_train, y_train, X_validation, original_dataset, test_set
 
 
 def GBM(X, y):
-    clf = GradientBoostingRegressor(random_state=None, n_estimators=1500, max_depth=2,
-                                    min_samples_split=2, learning_rate=0.01,
+    clf = GradientBoostingRegressor(max_features="log2", random_state=8, n_estimators=1500, max_depth=2,
+                                    min_samples_split=2, min_samples_leaf=1, learning_rate=0.01,
                                     loss='quantile', alpha=0.7)
     clf.fit(X, y)
-    print('Feature importances GBM: {}'.format(clf.feature_importances_))
-    
+    joblib.dump(clf, '../results/GBM_model.pkl') 
+    print('Feature importances GBM: {}'.format(clf.feature_importances_), X.columns.values)
+    plot_feature_importances(clf.feature_importances_, X.columns.values)
+    print(clf)
     return clf
 
 
 def RF(X, y):
-    clf = RandomForestRegressor(n_estimators=10, max_depth=None, min_samples_split=2,
-                                random_state=4, max_features='sqrt', n_jobs=1)
+    clf = RandomForestRegressor(n_estimators=20, max_depth=None, min_samples_split=2, min_samples_leaf=1,
+                                random_state=4, max_features='log2', n_jobs=1)
     clf.fit(X, y)
+    joblib.dump(clf, '../results/RF_model.pkl') 
     print('Feature importances RF: {}'.format(clf.feature_importances_))
-    
+    plot_feature_importances(clf.feature_importances_, X.columns.values)
+    print(clf)
     return clf
 
 
@@ -63,8 +66,6 @@ def predictions(original_dataset, test_set, clfrf, clfgbm):
         outputgbm = clfgbm.predict(test_row)
         predictionsrf.append(outputrf)
         predictionsgbm.append(outputgbm)
-        if i == 5000:
-            break
 
     rf = 'RF'
     gbm = 'GBM'
@@ -102,9 +103,6 @@ def nDCG(predictions, used_properties, original_dataset, method):
                 max_score = calculate_score(listings, None, None)
                 final_score = score / max_score
             random_score_list.append(final_score)
-
-            if i == 5000:
-                break
 
     mean_final_scores = np.mean(final_score_list)
     print('Final score {}: {}'.format(method, mean_final_scores))
@@ -169,22 +167,33 @@ def predict_test_set(clf, test_set):
                 submission.write(
                     str(int(listing.srch_id)) + ',' + str(int(listing.prop_id)) + '\n')
 
-
+def plot_feature_importances(importances, values):
+    feature_importance = 100.0 * (importances / importances.max())
+    sorted_idx = np.argsort(feature_importance)
+    pos = np.arange(sorted_idx.shape[0]) + .5
+    plt.subplot(1, 2, 2)
+    plt.barh(pos, feature_importance[sorted_idx], align='center')
+    plt.yticks(pos, values[sorted_idx])
+    plt.xlabel('Relative Importance')
+    plt.title('Feature Importance')
+    plt.show()
+        
+    
 def main():
     # Set useless warning to None
     pd.options.mode.chained_assignment = None
 
     # main program
     X_train, y_train, X_validation, original_dataset, test_set = import_data()
-    # clfgbm = GBM(X_train.drop('prop_id', axis=1), y_train)
+    clfgbm = GBM(X_train.drop('prop_id', axis=1), y_train)
     clfrf = RF(X_train.drop('prop_id', axis=1), y_train)
-    # nDCG_RF, nDCG_GBM = predictions(original_dataset, X_validation, clfrf, clfgbm)
+    nDCG_RF, nDCG_GBM = predictions(original_dataset, X_validation, clfrf, clfgbm)
 
-    # if nDCG_RF > nDCG_GBM:
-    predict_test_set(clfrf, test_set)
-    # else:
-    #     predict_test_set(clfgbm, test_set)
-    # predict test set with the best model
+    if nDCG_RF > nDCG_GBM:
+        predict_test_set(clfrf, test_set)
+    else:
+        predict_test_set(clfgbm, test_set)
+    #predict test set with the best model
 
 
 if __name__ == '__main__':
